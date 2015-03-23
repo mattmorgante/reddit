2a7a233689efe5cc1a40126354710a6238dd3355
Matt <matthewmorgante@gmail.com>
2015-03-23 10:35:38 +0700

    validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   validates :body, presence: true 
   validates :title, presence: true
+  validates :title, length: { maximum: 140 }
   belongs_to :user
   has_many :comments
 end
