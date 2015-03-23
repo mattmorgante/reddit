44690788547e80e4373520c231aa1841a4100b0a
Matt <matthewmorgante@gmail.com>
2015-03-23 10:18:28 +0700

    micro reddit

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ActiveRecord::Base
+  validates :body, presence: true 
+  validates :title, presence: true
+  belongs_to :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :username, length: { in: 5..20}
+  has_many :posts
+end
